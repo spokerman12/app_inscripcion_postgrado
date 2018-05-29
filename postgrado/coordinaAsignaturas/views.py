670cef1f7d19807ca5ee24fc49970a0b34a933b8
gnus19 <37775126+gnus19@users.noreply.github.com>
2018-05-29 17:36:36 -0400

    Add files via upload

--- postgrado/coordinaAsignaturas/views.py
+++ postgrado/coordinaAsignaturas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+from django.template import loader
+from coordinaAsignaturas.models import *
 # Create your views here.
 def home(request):
 
@@ -8,3 +9,12 @@ def home(request):
 
 	args = {'myName': name, 'numbers': numbers}
 	return render(request, 'coordinaAsignaturas/login.html', args)
+
+def vistaOfertas(request, oferta_id):
+	return HttpResponse("Estas en la vista de oferta %s" % oferta_id)
+	#ultimasOfertas = Oferta.objects
+	#template = loader.get_template('coordinaAsignaturas/oferta.html')
+	#context = {
+	#	'ultimasOfertas' : ultimasOfertas,
+	#}
+	#return render(request, 'coordinaAsignaturas/oferta.html', context)
