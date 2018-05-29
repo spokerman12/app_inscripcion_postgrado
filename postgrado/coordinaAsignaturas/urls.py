00330d0bcca8f460bc6e983d26c4635426ccbe83
gnus19 <37775126+gnus19@users.noreply.github.com>
2018-05-29 19:35:06 -0400

    Add files via upload

--- postgrado/coordinaAsignaturas/urls.py
+++ postgrado/coordinaAsignaturas/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls import url
 from django.contrib.auth.views import login, logout
 from django.urls import path
 
+app_name = 'oferta'
+
 urlpatterns = [
 	url(r'^$', views.home),
 	url(r'^login/$', login, {'template_name': 'coordinaAsignaturas/login.html'}),
 	url(r'^logout/$', login, {'template_name': 'coordinaAsignaturas/logout.html'}),
-	path('<int:oferta_id>/', views.vistaOfertas, name='oferta')
-
+	path('oferta/', views.vistaOfertas, name='oferta'),
+	path('oferta/<int:oferta_id>/', views.detallesOferta, name='detalles')
 
 ]
--- postgrado/coordinaAsignaturas/views.py
+++ postgrado/coordinaAsignaturas/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.template import loader
 from coordinaAsignaturas.models import *
+from django.http import Http404
 # Create your views here.
 def home(request):
 
@@ -10,11 +11,15 @@ def home(request):
 	args = {'myName': name, 'numbers': numbers}
 	return render(request, 'coordinaAsignaturas/login.html', args)
 
-def vistaOfertas(request, oferta_id):
-	return HttpResponse("Estas en la vista de oferta %s" % oferta_id)
-	#ultimasOfertas = Oferta.objects
-	#template = loader.get_template('coordinaAsignaturas/oferta.html')
-	#context = {
-	#	'ultimasOfertas' : ultimasOfertas,
-	#}
-	#return render(request, 'coordinaAsignaturas/oferta.html', context)
+def vistaOfertas(request):
+	#return HttpResponse("Estas en la vista de oferta %s" % oferta_id)
+	ultimasOfertas = Oferta.objects.order_by('id')
+	template = loader.get_template('coordinaAsignaturas/oferta.html')
+	context = {
+		'ultimasOfertas' : ultimasOfertas,
+	}
+	return render(request, 'coordinaAsignaturas/oferta.html', context)
+
+def detallesOferta(request, oferta_id):
+	oferta = get_object_or_404(Oferta, pk=oferta_id)
+	return render(request, 'coordinaAsignaturas/detallesOferta.html', {'oferta' : oferta})
